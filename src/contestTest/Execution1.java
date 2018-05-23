b79253f6fab435acfc642be871e29feeb518b943
Oleksii Kletsov <akletsov88@gmail.com>
2018-05-22 23:46:47 -0400

    inspecting every item and getting item competitors

--- src/contestTest/Execution1.java
+++ src/contestTest/Execution1.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 
 public class Execution1 {
@@ -17,6 +16,9 @@ public class Execution1 {
         driver.manage().window().maximize();
 
         String baseURL = "http://www.oddsportal.com/";
+        List <WebElement>  predictions;
+        String itemID;
+        String itemText;
         driver.get(baseURL);
 
         Creds credentials = new Creds();
@@ -37,20 +39,22 @@ public class Execution1 {
         driver.findElement(By.className("view-more")).click();
         Thread.sleep(3000);
 
-        List <WebElement> predictions = driver.findElements(By.cssSelector(".feed-item"));
+        predictions = driver.findElements(By.cssSelector(".feed-item"));
         // List predictions = driver.findElements(By.xpath("//*[@class='feed-item']"));
 
         System.out.println("The number of predictions found is: " + predictions.size());
 
-        String text = driver.findElement(By.cssSelector("#feed_item_2887498303 .odd a")).getText();
-        // String text = driver.findElement(By.xpath("//*[@id='feed_item_2887498303']//*[@class='odd']//a[1]")).getText();
-
-        System.out.println("Captured text is: " + text);
+//        itemText = driver.findElement(By.cssSelector("#feed_item_2887498303 .odd a")).getText();
+//        // String text = driver.findElement(By.xpath("//*[@id='feed_item_2887498303']//*[@class='odd']//a[1]")).getText();
+//
+//        System.out.println("Captured text is: " + itemText);
 
         // Getting the list of feed item IDs (to inspect elements within them in future)
         for (int i = 0; i < predictions.size(); i++) {
-            String itemID = predictions.get(i).getAttribute("id");
-            System.out.println(itemID);
+            itemID = predictions.get(i).getAttribute("id");
+//            System.out.println(itemID);
+            itemText = driver.findElement(By.cssSelector("#" + itemID + " .odd a")).getText();
+            System.out.println(itemText);
         }
     }
 }
