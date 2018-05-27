73ad9a8ff142354fc117fce13d4f39e1e35a1047
Oleksii Kletsov <akletsov88@gmail.com>
2018-05-27 18:44:12 -0400

    Enhancing generic method to find list of elements

--- src/usefulMethods/GenericMethods.java
+++ src/usefulMethods/GenericMethods.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class GenericMethods {
     WebDriver driver;
 
@@ -20,6 +22,30 @@ public class GenericMethods {
         else if (type.equals("xpath")) {
             System.out.println("Element found with xpath: " + locator);
             return this.driver.findElement(By.xpath(locator));
+        }
+        else if (type.equals("css")){
+            System.out.println("Element found with css: " + locator);
+            return  this.driver.findElement(By.cssSelector(locator));
+        }
+        else {
+            System.out.println("Locator type not supported");
+            return null;
+        }
+    }
+
+    public List<WebElement> getElementsList(String locator, String type){
+        type = type.toLowerCase();
+        if (type.equals("id")) {
+            System.out.println("Elements found with id: " + locator);
+            return this.driver.findElements(By.id(locator));
+        }
+        else if (type.equals("xpath")) {
+            System.out.println("Elements found with xpath: " + locator);
+            return this.driver.findElements(By.xpath(locator));
+        }
+        else if (type.equals("css")){
+            System.out.println("Elements found with css: " + locator);
+            return  this.driver.findElements(By.cssSelector(locator));
         }
         else {
             System.out.println("Locator type not supported");
