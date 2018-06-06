a778032e5ffb444181f25c26e10a2656d36753a5
Oleksii Kletsov <okletsov@cainc.com>
2018-06-06 16:30:28 -0400

    Added first TestNG class

---
+++ src/testNG/appCode/SomeClassToTest.java
@@ -0,0 +1,7 @@
+package testNG.appCode;
+
+public class SomeClassToTest {
+	public int sumNumbers(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/testNG/testClasses/TestAnnotation.java
@@ -0,0 +1,24 @@
+package testNG.testClasses;
+
+import org.testng.annotations.Test;
+import testNG.appCode.SomeClassToTest;
+
+public class TestAnnotation {
+
+	@Test
+	public void testMethod1(){
+		SomeClassToTest obj = new SomeClassToTest();
+		int result = obj.sumNumbers(1,2);
+		System.out.println("Running test --> testMethod1");
+	}
+
+	@Test
+	public void testMethod2(){
+		System.out.println("Running test --> testMethod2");
+	}
+
+	@Test
+	public void testMethod3(){
+		System.out.println("Running test --> testMethod3");
+	}
+}
