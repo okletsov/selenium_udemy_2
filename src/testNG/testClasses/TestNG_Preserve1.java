64f9e493f7ccc5a7e882ad8c86a7500ee051f20c
Oleksii Kletsov <okletsov@cainc.com>
2018-06-12 09:19:27 -0400

    Added preserve-order xml to demo how to preserve order of tests written in xml and how to disable tests in xml

---
+++ src/testNG/testClasses/TestNG_Preserve1.java
@@ -0,0 +1,15 @@
+package testNG.testClasses;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Preserve1 {
+	@Test
+	public void testMethod1(){
+		System.out.println("TestNG_Preserve1: Running testMethod1");
+	}
+
+	@Test
+	public void testMethod2(){
+		System.out.println("TestNG_Preserve1: Running testMethod2");
+	}
+}
---
+++ src/testNG/testClasses/TestNG_Preserve2.java
@@ -0,0 +1,15 @@
+package testNG.testClasses;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Preserve2 {
+	@Test
+	public void testMethod1(){
+		System.out.println("TestNG_Preserve2: Running testMethod1");
+	}
+
+	@Test
+	public void testMethod2(){
+		System.out.println("TestNG_Preserve2: Running testMethod2");
+	}
+}
