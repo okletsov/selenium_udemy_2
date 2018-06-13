b36db33ec5c36d861f787823340e5f07e189b3f1
Oleksii Kletsov <okletsov@cainc.com>
2018-06-13 16:02:36 -0400

    Added ITestResults demo to capture status of test method (fail or not)

---
+++ src/testNG/testClasses/TestNG_ITestResult.java
@@ -0,0 +1,29 @@
+package testNG.testClasses;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class TestNG_ITestResult {
+	@Test
+	public void testMethod1(){
+		System.out.println("TestNG_ITestResult: Running testMethod1");
+		Assert.assertTrue(false);
+	}
+
+	@Test
+	public void testMethod2(){
+		System.out.println("TestNG_ITestResult: Running testMethod2");
+		Assert.assertTrue(true);
+	}
+
+	@AfterMethod
+	public void afterDemo(ITestResult testResult){
+		if(testResult.getStatus()== ITestResult.FAILURE){
+			System.out.println("Failed: " + testResult.getMethod().getMethodName());
+		} else if (testResult.getStatus()== ITestResult.SUCCESS){
+			System.out.println("Succeed:" + testResult.getName());
+		}
+	}
+}
