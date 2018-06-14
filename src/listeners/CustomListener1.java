58fe9fd82715777d9a6cf791497d4e119c875f27
Oleksii Kletsov <akletsov88@gmail.com>
2018-06-13 23:25:48 -0400

    Added implementation of the custom listener

---
+++ src/listeners/CustomListener1.java
@@ -0,0 +1,20 @@
+package listeners;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class CustomListener1 implements IInvokedMethodListener {
+
+    @Override
+    public void beforeInvocation(IInvokedMethod iInvokedMethod, ITestResult iTestResult) {
+        // Runs before every method in Test Class
+        System.out.println("beforeInvocation: " + iTestResult.getTestClass().getName() + "-> " + iInvokedMethod.getTestMethod().getMethodName());
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod iInvokedMethod, ITestResult iTestResult) {
+        // Runs after every method in Test Class
+        System.out.println("afterInvocation: " + iTestResult.getTestClass().getName() + "-> " + iInvokedMethod.getTestMethod().getMethodName());
+    }
+}
---
+++ src/testNG/testClasses/TestNG_Listeners1.java
@@ -0,0 +1,31 @@
+package testNG.testClasses;
+
+import listeners.CustomListener1;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+@Listeners(CustomListener1.class)
+public class TestNG_Listeners1 {
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("TestNG_Listeners1: This runs once before class");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("TestNG_Listeners1: This runs once after class");
+    }
+
+    @Test
+    public void testMethod1(){
+        System.out.println("TestNG_Listeners1: Running testMethod1");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testMethod2(){
+        System.out.println("TestNG_Listeners1: Running testMethod2");
+        Assert.assertTrue(true);
+    }
+}
