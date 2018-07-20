29a9ec18abc408240624b90940816113ed9c9d39
Oleksii Kletsov <okletsov@cainc.com>
2018-07-20 14:43:45 -0400

    Added sample Creds.java file

---
+++ src/test/java/com/okletsov/contestTest/Creds.java
@@ -0,0 +1,14 @@
+package com.okletsov.contestTest;
+
+public class Creds {
+    private String username = "username";
+    private String password = "password";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
